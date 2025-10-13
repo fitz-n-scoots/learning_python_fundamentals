9e1086654b8f032f6dffa0f273f4e135bf9fad68
fitz-n-scoots <victor.edsall1@gmail.com>
2025-10-13 09:46:22 -0300

    learning_python_fundamentals

--- examples/chapter#4/chapter#4examples.py
+++ examples/chapter#4/chapter#4examples.py
@@ -5,18 +5,27 @@ def pets():
   pets('are you a dog re you a cat?')  
 
 
-# what is an if or else
+# what is an if
 
 # you ask it somthing and it gives you somthing else
-count = ('1,2,3,4,')  
-if len(count) >5: 
-    print('thats long')  
+name = ('sebastian')  
+if len(name) >7: 
+    print(name+' is a long name')
+  
+# what is else
+
+# its like if but the opposite
+
+if (len)victor < (len)jack:
+  print("victor is shorter than jack")
+else:
+  print("jack is shorter than victor")
 
 # what is elif
     
 # elif is both combined
-if b > a:
-  print("b is greater than a")
-elif a == b:
-  print("a and b are equal")
+if 1 > 1:
+  print("1 is greater than 1")
+elif 1 == 1:
+  print("1 and 1 are equal")
   
