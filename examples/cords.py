fc92fe4d9e90e537b0977cc33063dc1a521f1f7e
fitz-n-scoots <victor.edsall1@gmail.com>
2025-11-03 19:27:21 -0400

    a cords example

--- examples/cords.py
+++ examples/cords.py
@@ -1,27 +1,14 @@
-#first try didn't work
+class Coords:
+    def __init__(self, right=0, left=180, up=90):
+        self.right = right
+        self.left = left
+        self.up = up
 
-
-# import turtle
-# t = turtle.Turtle()
-# t.reset
-# for x in range(1,2):
-	# self = t.forward(100)
-	# self
-# turtle.done()
-	
-# class Coords:
-	# def _int_(self, x1=20, y1=67, x2=0, y2=0):
-	    # self.x1 = x1
-	    # self.y1 = y1
-	    # self.x2 = x2
-	    # self.y2 = y2
-
-
-#second try worked I think on the first try I kindof was thinking about it wrong.
-# import numpy
-import numpy as np
-a = np.array([1, 2, 3])
-# using np.coords() method
-gfg = a.flat
-next(gfg)
-print(gfg.coords)
+import turtle
+t = turtle.Turtle()
+t.reset
+for x in range(1,2):
+    position = Coords()
+    t.left(position.right)
+    t.forward(100)
+turtle.done()
